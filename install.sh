c42e930f51f78cf0b363c60bd342086b79943867
Dario Furlan <dario.furlan.51@gmail.com>
2018-11-07 23:03:28 +0100

    updates on installer

--- install.sh
+++ install.sh
@@ -1,1 +1,18 @@
 #!/usr/bin/env bash
+
+if [ "$EUID" -ne 0 ]
+then
+    echo "Please run as root"
+    exit
+fi
+
+# todo check if line is already written if not write the newline
+log='$(whoami) [$$]: $(history 1 | sed "s/^[ ]*[0-9]\+[ ]*//" ) [$RETRN_VAL]'
+newline1="export PROMPT_COMMAND='RETRN_VAL=$?;logger -p local6.debug "${log}"'"
+#echo "$newline1" >> /etc/bash.bashrc
+echo $newline1
+
+newline2="local6.*    /var/log/commands.log"
+#echo "$newline2" >> /etc/rsyslog.d/bash.conf
+
+# login hook da mettere in /etc/profile.d/[script].sh
