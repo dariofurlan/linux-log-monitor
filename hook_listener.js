be267464b73dcd2a3a0dad67c0b12c571811ab89
Dario Furlan <dario.furlan.51@gmail.com>
2018-11-13 18:41:02 +0100

    bash part working properly

--- hook_listener.js
+++ hook_listener.js
@@ -1,18 +1,59 @@
 const net = require('net');
-
-// to send message  from bash
-/*
- * echo "{\"event\":\"login\"}" >/dev/tcp/127.0.0.1/9000
- */
-
-
-const server = net.createServer((sock) => {
-    sock.on('data', data => {
-        let js = JSON.parse(data);
-        console.log(js["event"]);
+const server = require('http').createServer();
+const io = require('socket.io')(server, {
+    origins: '*:*',
+    serveClient: false,
+    pingInterval: 10000,
+    pingTimeout: 5000,
+    cookie: false
+});
+const ports = {
+    socketio : 9001,
+    hook_server: 9000
+};
+const rooms = ["commands", "login"];
+io.on('connection', socket => {
+    socket.on('join', room => {
+        if (rooms.includes(room)) {
+            console.log("user joined room: " + room);
+            socket.join(room);
+        } else {
+            console.log("wrong room");
+            socket.emit("exception", "wrong room");
+        }
+    });
+    socket.on('leave', room => {
+        if (rooms.includes(room)) {
+            socket.leave(room, () => {
+            });
+            console.log("user left %s" % room);
+        } else {
+            console.log("wrong room");
+            socket.emit("exception", "wrong room");
+        }
     });
 });
 
-server.listen({port: 9000}, () => {
-    console.log('Server is listening');
+
+const local_hook_server = net.createServer((sock) => {
+    sock.on('data', data => {
+        try {
+            let js = JSON.parse(data);
+            let event = js["event"];
+            console.log(event);
+            console.log(js.data);
+            if (rooms.includes(event))
+                io.sockets.to(event).emit(event, data/*todo message to send...*/);
+        } catch (e) {
+            console.error(e);
+            console.error(data.toString('utf8'));
+        }
+    });
+});
+
+server.listen(ports.socketio,()=> {
+    console.log("Socketi.IO is listening on port: " + ports.socketio);
+});
+local_hook_server.listen({port: ports.hook_server}, () => {
+    console.log("Hook Server is listening on port: " + ports.hook_server);
 });
--- install.sh
+++ install.sh
@@ -10,18 +10,31 @@ is_string_to_write() {
     fi
 }
 
+esc() {
+    printf "%s\n" "$1" | sed -e "s/'/'\"'\"'/g" -e "1s/^/'/" -e "\$s/\$/'/"
+}
+json_escape () {
+    printf '%s' "$1" | python -c 'import json,sys; print(json.dumps(sys.stdin.read()))'
+}
 
 if [[ "$EUID" -ne 0 ]]
 then
     echo "Please run as root"
     exit
 fi
-log_hook='$((echo "{\"event\":\"command\"}" >/dev/tcp/127.0.0.1/9000) 2>/dev/null)'
-log='$(whoami) [$$]: $(history 1 | sed "s/^[ ]*[0-9]\+[ ]*//" ) [$RETRN_VAL]'
-newline1="export PROMPT_COMMAND='RETRN_VAL=$?;logger -p local6.debug \"${log_hook} ${log}\"'"
+# todo: instead of making more difficult than it is use
+# todo: tail -1 /var/log/commands.log
+# todo: so that we still get the last command at least in teory...
+
+log_hook='$((echo "{\"event\":\"commands\",\"data\":{\"command\":$(esc "$LAST_CMD")}}" >/dev/tcp/127.0.0.1/9000 ) 2>/dev/null)'
+log='$(whoami) [$$]: $LAST_CMD [$RETRN_VAL]'
+newline1='export PROMPT_COMMAND='"'"'RETRN_VAL=$?;LAST_CMD=$(history 1 | sed "s/^[ ]*[0-9]\+[ ]*//");logger -p local6.debug '"${log}"';'"$log_hook';"
 if is_string_to_write "$newline1" "/etc/bash.bashrc"
 then
     echo "added command hook pt.1"
+    echo 'esc () {
+    printf '"'"'%s'"'"' "$1" | python -c '"'"'import json,sys; print(json.dumps(sys.stdin.read()))'"'"'
+}' >> /etc/bash.bashrc
     echo "$newline1" >> /etc/bash.bashrc
 fi
 newline2="local6.*    /var/log/commands.log"
@@ -31,8 +44,8 @@ then
     echo "$newline2" >> /etc/rsyslog.d/bash.conf
 fi
 
-file_login_hook="/etc/profile.d/login_hook.sh"
 login_hook='(echo "{\"event\":\"login\"}" >/dev/tcp/127.0.0.1/9000) 2>/dev/null'
+file_login_hook="/etc/profile.d/login_hook.sh"
 if [[ ! -e ${file_login_hook} ]]
 then
     if is_string_to_write ${login_hook} ${file_login_hook}
@@ -41,6 +54,3 @@ then
         echo ${login_hook} > ${file_login_hook}
     fi
 fi
-
-# todo una grande libreria bash con tutte le funzioni per l'ambiente
-# todo login hook da mettere in /etc/profile.d/[script].sh
